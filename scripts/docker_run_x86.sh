d1634e1ebd92070060b642d847a44436a7fed5ad
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2022-06-23 08:25:48 +0000

    x86_64 support

---
+++ scripts/docker_run_x86.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+IMAGE_TAG="22.05-py3"
+if [ ${#} -gt 0 ]
+then
+    IMAGE_TAG=${1}
+fi
+
+sudo docker run \
+    -it \
+    --rm \
+    --net=host \
+    --gpus all \
+    --ipc=host \
+    --ulimit memlock=-1 \
+    --ulimit stack=67108864 \
+    torchvision2trt-samples:${IMAGE_TAG}
