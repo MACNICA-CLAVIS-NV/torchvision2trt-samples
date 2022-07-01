d85f3a239672c967f2dc1cff8e5c39ed2613ca9e
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2022-07-01 11:56:03 +0900

    Fixed for L4T 32.7.2

--- scripts/docker_base.sh
+++ scripts/docker_base.sh
@@ -12,6 +12,10 @@ if [ $L4T_RELEASE -eq 32 ]; then
 		elif [ $L4T_REVISION_MINOR -eq 2 ]; then
 			BASE_IMAGE="nvcr.io/nvidia/l4t-ml:r32.5.0-py3"
 		fi
+    elif [ $L4T_REVISION_MAJOR -eq 7 ]; then
+        if [ $L4T_REVISION_MINOR -eq 2 ]; then
+            BASE_IMAGE="nvcr.io/nvidia/l4t-ml:r32.7.1-py3"
+        fi
 	fi
 fi
 	
